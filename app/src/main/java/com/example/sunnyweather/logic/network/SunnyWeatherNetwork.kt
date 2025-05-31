7ea84cedffa839220942ff9937099d67ceafa23e
Mjw <3080870193@qq.com>
2025-05-31 17:21:13 +0800

    实现搜索全球城市数据功能。

---
+++ app/src/main/java/com/example/sunnyweather/logic/network/SunnyWeatherNetwork.kt
@@ -0,0 +1,29 @@
+package com.example.sunnyweather.logic.network
+
+import kotlinx.coroutines.suspendCancellableCoroutine
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+object SunnyWeatherNetwork {
+    private val placeService = ServiceCreator.create<PlaceService>()
+    suspend fun searchPlaces(query: String) = placeService.searchPlaces(query).await()
+    private suspend fun <T> Call<T>.await(): T {
+        return suspendCancellableCoroutine { continuation ->
+            enqueue(object : Callback<T> {
+                override fun onResponse(call: Call<T>, response: Response<T>) {
+                    val body = response.body()
+                    if (body != null) continuation.resume(body)
+                    else continuation.resumeWithException(
+                        RuntimeException("response body is null"))
+                }
+                override fun onFailure(call: Call<T>, t: Throwable) {
+                    continuation.resumeWithException(t)
+                }
+            })
+        }
+    }
+}
